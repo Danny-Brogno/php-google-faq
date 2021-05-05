8db433c263c68d89ca4075809d9ae24cc890ddd7
Danny <danny.brogno@gmail.com>
2021-05-05 16:22:01 +0200

    mini-changes

--- index.php
+++ index.php
@@ -7,16 +7,30 @@
   <body>
     <style>
 
-      body {
-        
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+      }
+
+      li {
+        list-style: none;
+      }
+
+      a {
+        text-decoration: none;
       }
 
       header, main {
-        color: white;
         display: flex;
         justify-content: center;
         align-items: center;
         flex-direction: column;
+        color: black;
+      }
+
+      .test {
+        color: blue;
       }
 
     </style>
@@ -183,6 +197,46 @@
           </h3>
     </section> -->
 
+    <main>
+      <?php
+        // GOAL: riscrivere questa pagina del sito google
+        // Ci sono diverse domande con relative risposte.
+        // Gestire il "Database" e la visualizzazione di
+        // queste domande e risposte con PHP.
+
+        $googleDatabase = [
+
+          [
+            "question" => "<h2>" . 'How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?' . "</h2>",
+            "answers" =>
+              [
+                  "The recent ruling by the Court of Justice of the European Union has profound consequences for <span class='test'> search </span> engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.",
+                  "Since this ruling was published on 13 May 2014, we've been working around the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with the public's right to know and distribute information.",
+                  "If you have a removal request, please fill out this web form. You'll receive an automatic reply confirming that we have received your request. We will then assess your case – please note that this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results – for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organisation, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA",
+                  "We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe that it's important to respect the Court's judgement and we are working hard to devise a process that complies with the law.",
+                  "When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal."
+              ]
+
+          ]   // END OF FIRST PARAGRAPH
+
+        ]; // END OF $googleDatabase
+
+          foreach ($googleDatabase as $content) {
+            echo $content["question"];
+            foreach ($content as $answer ) {
+              echo $answer[0];
+            }
+          }
+
+          // foreach ($posts as $date => $datePosts) {
+          //   echo $date;
+          //   foreach ($datePosts as $post ) {
+          //     echo " <br> " . $post["title"] . " - Autore: " . $post["author"] . " - testo: " . $post["text"] . "<br>";
+          //   }
+          //   echo "<br>";
+          // }
+        ?>
+    </main>
 
   </body>
 
